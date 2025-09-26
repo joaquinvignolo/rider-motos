6e11a82bdb2e2fa56c527869e61b9f7783077798
Kress <joacovignolo@gmail.com>
2025-09-25 21:28:43 -0300

    no se puede cancelar un tramite

--- src/components/Patentamiento.tsx
+++ src/components/Patentamiento.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Patentamiento.css";
 
-const estados = ["Todos los estados", "Pendiente", "Completado", "Cancelado"];
+const estados = ["Todos los estados", "Pendiente", "Completado"];
 
 const tramitesEjemplo = [
   {
