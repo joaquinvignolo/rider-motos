e136a264f0471f3f6daeeeadbbc1024c0700defa
Kress <joacovignolo@gmail.com>
2025-10-21 16:14:27 -0300

    up

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://rider-motos-ten.vercel.app/',
+  'https://rider-motos-ten.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
